bc3ce7095bcfbce1ec573b64621be50eac7721f5
Fred <fred.ng.27@gmail.com>
2017-03-28 21:01:33 -0400

    Creates Lidar Project Database
    This database has the link to the backup file with all the tiles merged
    into one table.

---
+++ db/create_lidarproject_db.sql
@@ -0,0 +1,14 @@
+-- Database: Lidar_project
+
+-- Backup restore is located here: https://drive.google.com/file/d/0B7Fxq2mWeyTyM1Nid05ibmtpc28/view?usp=sharing
+
+-- DROP DATABASE "Lidar_project";
+
+CREATE DATABASE "Lidar_project"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
